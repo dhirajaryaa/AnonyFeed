851cefddcc8c8ecb910ce8a0ae74fed1efbeb11b
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-06 04:18:40 +0530

    feat: Update routing with react-router and add protected routes for user dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { BrowserRouter, Routes, Route } from "react-router";
+import UserPage from "./pages/UserPage";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
-    <div>this is react
-      <Button>hello</Button>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* protected routes */}
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<UserPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router";
+
+function ProtectedRoutes() {
+  return <Outlet />;
+}
+
+export default ProtectedRoutes;
