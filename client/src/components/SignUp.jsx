030fb8f2f5f14a464f22d5257ddb03a994aa4b45
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-06 05:03:35 +0530

    feat: Implement signup form with loading state and card UI components

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,8 +1,56 @@
+import { TabsContent } from "@/components/ui/tabs";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
 
 function SignUp() {
+  const [loading, setLoading] = useState(false);
   return (
-    <div>SigfdasfdasnUp</div>
-  )
+    <TabsContent value="signup">
+      <form>
+        <Card>
+          <CardHeader>
+            <CardTitle>Signup</CardTitle>
+            <CardDescription>
+              Fill out the form below to create your account. Ensure all fields
+              are completed accurately.
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" type={"text"} placeholder="dhiraj" />
+              <div className="text-destructive">this username not ablible</div>
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type={"email"} placeholder="dhiraj@gmail.com" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type={"password"} placeholder="abc@123" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className={"w-full"}>
+              {loading ? <Loader2 className="size-6 animate-spin" /> : "SignUp"}
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </TabsContent>
+  );
 }
 
-export default SignUp
+export default SignUp;
--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,14 +1,13 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import React from "react";
 import { Outlet } from "react-router";
 
 function AuthPage() {
   return (
     <main className="w-full h-screen flex items-center justify-center">
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+      <Tabs defaultValue="signup" className="w-sm">
+        <TabsList className={"grid grid-cols-2 w-full h-14 p-2"}>
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+          <TabsTrigger value="signin">Signin</TabsTrigger>
         </TabsList>
         {/* tab content  */}
         <Outlet />
